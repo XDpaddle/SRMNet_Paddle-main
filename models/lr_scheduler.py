0363c9532ca2c891b3f79f6a67f3796b9c98699d
SiqiWang <zgwsq99@163.com>
2023-07-12 12:42:21 +0800

    first commit

--- models/lr_scheduler.py
+++ models/lr_scheduler.py
@@ -1,252 +1,69 @@
 import math
-from collections import Counter
-from collections import defaultdict
-import paddle
 from paddle.optimizer.lr import LRScheduler
 
-class MultiStepLR_Restart(LRScheduler):
-    def __init__(self, optimizer, milestones, restarts=None, weights=None, gamma=0.1,
-                 clear_state=False, last_epoch=-1):
-        self.milestones = Counter(milestones)
-        self.gamma = gamma
-        self.clear_state = clear_state
-        self.restarts = restarts if restarts else [0]
-        self.restarts = [v + 1 for v in self.restarts]
-        self.restart_weights = weights if weights else [1]
-        assert len(self.restarts) == len(
-            self.restart_weights), 'restarts and their weights do not match.'
-        super(MultiStepLR_Restart, self).__init__(optimizer, last_epoch)
 
-    def get_lr(self):
-        if self.last_epoch in self.restarts:
-            if self.clear_state:
-                self.optimizer.state = defaultdict(dict)
-            weight = self.restart_weights[self.restarts.index(self.last_epoch)]
-            return [group['initial_lr'] * weight for group in self.optimizer.param_groups]
-        if self.last_epoch not in self.milestones:
-            return [group['lr'] for group in self.optimizer.param_groups]
-        return [
-            group['lr'] * self.gamma**self.milestones[self.last_epoch]
-            for group in self.optimizer.param_groups
-        ]
+def get_position_from_periods(iteration, cumulative_period):
+    """Get the position from a period list.
 
-
-class CosineAnnealingLR_Restart(LRScheduler):
-    def __init__(self, optimizer, T_period, restarts=None, weights=None, eta_min=0, last_epoch=-1):
-        self.T_period = T_period
-        self.T_max = self.T_period[0]  # current T period
-        self.eta_min = eta_min
-        self.restarts = restarts if restarts else [0]
-        self.restarts = [v + 1 for v in self.restarts]
-        self.restart_weights = weights if weights else [1]
-        self.last_restart = 0
-        self.optimizer = optimizer
-        self.optimizer._learning_rate = self
-        assert len(self.restarts) == len(
-            self.restart_weights), 'restarts and their weights do not match.'
-        super(CosineAnnealingLR_Restart, self).__init__(optimizer.get_lr(), last_epoch)
-
-    def get_lr(self):
-        if self.last_epoch == 0:
-            # return self.base_lrs
-            return [self.base_lr]
-        elif self.last_epoch in self.restarts:
-            self.last_restart = self.last_epoch
-            self.T_max = self.T_period[self.restarts.index(self.last_epoch) + 1]
-            weight = self.restart_weights[self.restarts.index(self.last_epoch)]
-            return [self.base_lr * weight ]
-        elif (self.last_epoch - self.last_restart - 1 - self.T_max) % (2 * self.T_max) == 0:
-            return [
-                self.optimizer.get_lr() + (self.base_lr - self.eta_min) * (1 - math.cos(math.pi / self.T_max)) / 2
-            ]
-        return [(1 + math.cos(math.pi * (self.last_epoch - self.last_restart) / self.T_max)) /
-                (1 + math.cos(math.pi * ((self.last_epoch - self.last_restart) - 1) / self.T_max)) *
-                (self.optimizer.get_lr()[0] - self.eta_min) + self.eta_min
-                ]
-                # for group in self.optimizer.param_groups]
-
-
-class CosineAnnealingDecay(LRScheduler):
-    r"""
-    Set the learning rate using a cosine annealing schedule, where :math:`\eta_{max}` is set to
-    the initial learning_rate. :math:`T_{cur}` is the number of epochs since the last restart in
-    SGDR.
-    The algorithm can be described as following.
-    .. math::
-        \eta_t & = \eta_{min} + \frac{1}{2}(\eta_{max} - \eta_{min})\left(1
-        + \cos\left(\frac{T_{cur}}{T_{max}}\pi\right)\right),
-        & T_{cur} \neq (2k+1)T_{max};
-        \eta_{t+1} & = \eta_{t} + \frac{1}{2}(\eta_{max} - \eta_{min})
-        \left(1 - \cos\left(\frac{1}{T_{max}}\pi\right)\right),
-        & T_{cur} = (2k+1)T_{max}.
-
-    It has been proposed in `SGDR: Stochastic Gradient Descent with Warm Restarts <https://arxiv.org/abs/1608.03983>`_.
-    Note that this only implements the cosine annealing part of SGDR, and not the restarts.
+    It will return the index of the right-closest number in the period list.
+    For example, the cumulative_period = [100, 200, 300, 400],
+    if iteration == 50, return 0;
+    if iteration == 210, return 2;
+    if iteration == 300, return 2.
 
     Args:
-        learning_rate (float): The initial learning rate, that is :math:`\eta_{max}` . It can be set to python float or int number.
-        T_max (int): Maximum number of iterations. It is half of the decay cycle of learning rate. It must be a positive integer.
-        eta_min (float|int, optional): Minimum learning rate, that is :math:`\eta_{min}` . Default: 0.
-        last_epoch (int, optional):  The index of last epoch. Can be set to restart training. Default: -1, means initial learning rate.
-        verbose (bool, optional): If ``True``, prints a message to stdout for each update. Default: ``False`` .
-    Returns:
-        ``CosineAnnealingDecay`` instance to schedule learning rate.
-    Examples:
+        iteration (int): Current iteration.
+        cumulative_period (list[int]): Cumulative period list.
 
-        .. code-block:: python
-            import paddle
-            import numpy as np
-            # train on default dynamic graph mode
-            linear = paddle.nn.Linear(10, 10)
-            scheduler = paddle.optimizer.lr.CosineAnnealingDecay(learning_rate=0.5, T_max=10, verbose=True)
-            sgd = paddle.optimizer.SGD(learning_rate=scheduler, parameters=linear.parameters())
-            for epoch in range(20):
-                for batch_id in range(5):
-                    x = paddle.uniform([10, 10])
-                    out = linear(x)
-                    loss = paddle.mean(out)
-                    loss.backward()
-                    sgd.step()
-                    sgd.clear_gradients()
-                    scheduler.step()    # If you update learning rate each step
-              # scheduler.step()        # If you update learning rate each epoch
-            # train on static graph mode
-            paddle.enable_static()
-            main_prog = paddle.static.Program()
-            start_prog = paddle.static.Program()
-            with paddle.static.program_guard(main_prog, start_prog):
-                x = paddle.static.data(name='x', shape=[None, 4, 5])
-                y = paddle.static.data(name='y', shape=[None, 4, 5])
-                z = paddle.static.nn.fc(x, 100)
-                loss = paddle.mean(z)
-                scheduler = paddle.optimizer.lr.CosineAnnealingDecay(learning_rate=0.5, T_max=10, verbose=True)
-                sgd = paddle.optimizer.SGD(learning_rate=scheduler)
-                sgd.minimize(loss)
-            exe = paddle.static.Executor()
-            exe.run(start_prog)
-            for epoch in range(20):
-                for batch_id in range(5):
-                    out = exe.run(
-                        main_prog,
-                        feed={
-                            'x': np.random.randn(3, 4, 5).astype('float32'),
-                            'y': np.random.randn(3, 4, 5).astype('float32')
-                        },
-                        fetch_list=loss.name)
-                    scheduler.step()    # If you update learning rate each step
-              # scheduler.step()        # If you update learning rate each epoch
+    Returns:
+        int: The position of the right-closest number in the period list.
+    """
+    for i, period in enumerate(cumulative_period):
+        if iteration <= period:
+            return i
+
+class CosineAnnealingRestartCyclicLR(LRScheduler):
+    """ Cosine annealing with restarts learning rate scheme.
+    An example of config:
+    periods = [10, 10, 10, 10]
+    restart_weights = [1, 0.5, 0.5, 0.5]
+    eta_min=1e-7
+    It has four cycles, each has 10 iterations. At 10th, 20th, 30th, the
+    scheduler will restart with the weights in restart_weights.
+    Args:
+        optimizer (torch.nn.optimizer): Torch optimizer.
+        periods (list): Period for each cosine anneling cycle.
+        restart_weights (list): Restart weights at each restart iteration.
+            Default: [1].
+        eta_min (float): The mimimum lr. Default: 0.
+        last_epoch (int): Used in _LRScheduler. Default: -1.
     """
 
     def __init__(self,
                  learning_rate,
-                 T_period,
-                 restarts=None,
-                 weights=None,
-                 eta_min=0,
-                 last_epoch=-1,
-                 verbose=False):
-        self.T_period = T_period
-        self.T_max = self.T_period[0]
-        self.eta_min = float(eta_min)
-        self.restarts = restarts if restarts else [0]
-        self.restarts = [v + 1 for v in self.restarts]
-        self.restart_weights = weights if weights else [1]
-        self.last_restart = 0
-        super(CosineAnnealingDecay, self).__init__(learning_rate, last_epoch,
-                                                   verbose)
+                 periods,
+                 restart_weights=(1, ),
+                 eta_mins=(0, ),
+                 last_epoch=-1):
+        self.periods = periods
+        self.restart_weights = restart_weights
+        self.eta_mins = eta_mins
+        assert (len(self.periods) == len(self.restart_weights)
+                ), 'periods and restart_weights should have the same length.'
+        self.cumulative_period = [
+            sum(self.periods[0:i + 1]) for i in range(0, len(self.periods))
+        ]
+        super(CosineAnnealingRestartCyclicLR, self).__init__(learning_rate, last_epoch)
+        
     def get_lr(self):
-        if self.last_epoch == 0:
-            return self.base_lr
-        elif self.last_epoch in self.restarts:
-            self.last_restart = self.last_epoch
-            self.T_max = self.T_period[self.restarts.index(self.last_epoch) + 1]
-            weight = self.restart_weights[self.restarts.index(self.last_epoch)]
-            return self.base_lr * weight
-        elif (self.last_epoch - self.last_restart - 1 - self.T_max) % (2 * self.T_max) == 0:
-            return self.last_lr + (self.base_lr - self.eta_min) * (1 - math.cos(math.pi/self.T_max)) / 2
-        return (1 + math.cos(math.pi * (self.last_epoch - self.last_restart) / self.T_max)) / (1 + math.cos(math.pi * ((self.last_epoch - self.last_restart) - 1) / self.T_max)) * (self.last_lr - self.eta_min) + self.eta_min
+        idx = get_position_from_periods(self.last_epoch,
+                                        self.cumulative_period)
+        current_weight = self.restart_weights[idx]
+        nearest_restart = 0 if idx == 0 else self.cumulative_period[idx - 1]
+        current_period = self.periods[idx]
+        eta_min = self.eta_mins[idx]
 
-    def _get_closed_form_lr(self):
-        return self.eta_min + (self.base_lr - self.eta_min) * (1 + math.cos(
-            math.pi * self.last_epoch / self.T_max)) / 2
+        return eta_min + current_weight * 0.5 * (self.base_lr - eta_min) * \
+            (1 + math.cos(math.pi * (
+                (self.last_epoch - nearest_restart) / current_period)))
 
-
-if __name__ == "__main__":
-    linear = paddle.nn.Linear(10, 10)
-    optimizer = paddle.optimizer.Adam(parameters=linear.parameters(), learning_rate=2e-4, weight_decay=0,
-                                 beta1=0.9, beta2=0.99)
-    ##############################
-    # MultiStepLR_Restart
-    ##############################
-    ## Original
-    lr_steps = [200000, 400000, 600000, 800000]
-    restarts = None
-    restart_weights = None
-
-    ## two
-    lr_steps = [100000, 200000, 300000, 400000, 490000, 600000, 700000, 800000, 900000, 990000]
-    restarts = [500000]
-    restart_weights = [1]
-
-    ## four
-    lr_steps = [
-        50000, 100000, 150000, 200000, 240000, 300000, 350000, 400000, 450000, 490000, 550000,
-        600000, 650000, 700000, 740000, 800000, 850000, 900000, 950000, 990000
-    ]
-    restarts = [250000, 500000, 750000]
-    restart_weights = [1, 1, 1]
-
-    # scheduler = MultiStepLR_Restart(optimizer, lr_steps, restarts, restart_weights, gamma=0.5,
-                                    # clear_state=False)
-
-    ##############################
-    # Cosine Annealing Restart
-    ##############################
-    ## two
-    T_period = [500000, 500000]
-    restarts = [500000]
-    restart_weights = [1]
-
-    ## four
-    T_period = [250000, 250000, 250000, 250000]
-    restarts = [250000, 500000, 750000]
-    restart_weights = [1, 1, 1]
-
-    scheduler = CosineAnnealingLR_Restart(optimizer, T_period, eta_min=1e-7, restarts=restarts,
-                                          weights=restart_weights)
-    optimizer._learning_rate = scheduler
-    ##############################
-    # Draw figure
-    ##############################
-    N_iter = 1000000
-    lr_l = list(range(N_iter))
-    for i in range(N_iter):
-        scheduler.step()
-        current_lr = optimizer.get_lr()
-        lr_l[i] = current_lr
-
-    import matplotlib as mpl
-    from matplotlib import pyplot as plt
-    import matplotlib.ticker as mtick
-    mpl.style.use('default')
-    import seaborn
-    seaborn.set(style='whitegrid')
-    seaborn.set_context('paper')
-
-    plt.figure(1)
-    plt.subplot(111)
-    plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
-    plt.title('Title', fontsize=16, color='k')
-    plt.plot(list(range(N_iter)), lr_l, linewidth=1.5, label='learning rate scheme')
-    legend = plt.legend(loc='upper right', shadow=False)
-    ax = plt.gca()
-    labels = ax.get_xticks().tolist()
-    for k, v in enumerate(labels):
-        labels[k] = str(int(v / 1000)) + 'K'
-    ax.set_xticklabels(labels)
-    ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.1e'))
-
-    ax.set_ylabel('Learning rate')
-    ax.set_xlabel('Iteration')
-    fig = plt.gcf()
-    plt.show()
